e83953b50d1af80ad2039c897ce34429712f0813
jackcrane <3jbc22@gmail.com>
2026-07-02 08:19:03 -0500

    update dates

--- api/process-email.js
+++ api/process-email.js
@@ -37,7 +37,7 @@ export async function generateEmail(id) {
 				id: shift.shift.job.id,
 				name: shift.shift.job.name,
 				location: shift.shift.job.location,
-				day: shift.shift.job.location.slug === 'Expo' ? 'Friday 8/1' : 'Saturday 8/2',
+				day: shift.shift.job.location.slug === 'Expo' ? 'Friday 7/31' : 'Saturday 8/1',
 				shifts: [
 					{
 						startTime: shift.shift.startTime,
